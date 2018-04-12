67951e9499ae2cf85652baf7990de4b2ce445d9b
GW <garviswbs@gmail.com>
2018-04-11 18:56:16 -0600

    pre depend update

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import './index.css';
+
+const App = () => {
+    return (<div> Test </div >);
+}
+
+ReactDom.render(<App />, document.querySelector('.container'));
+
+
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
